4a9db11867ab0b83d4b5588a36443475fbef954a
Gitea <gitea@fake.local>
2022-06-16 01:16:52 +0800

    Update SelfAuth.sol import path

--- web3/contracts/rsdefi/RSPay.sol
+++ web3/contracts/rsdefi/RSPay.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import './node_modules/@openzeppelin/contracts/utils/Address.sol';
 import './node_modules/@openzeppelin/contracts/access/Ownable.sol';
 import './node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol';
-import './SelfAuth.sol';
+import 'https://github.com/refitor/selfAuth/blob/v1.0.0/web3/contracts/rscore/SelfAuth.sol';
 
 // @title rs payment gateway inherited from OpenZeppelin Escrow
 // @author refitor
